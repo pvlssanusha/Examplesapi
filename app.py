df91dd136a31f71f5a23e29dfe924d5f1f7ab9af
anuperla <anushaperla2003@gmail.com>
2023-12-19 12:48:41 +0530

    initial changes

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+import os
+import requests
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+# Replace 'YOUR_COHERE_API_KEY' with your Cohere API key
+
+# Initialize the Cohere Client with an API Key
+api = os.getenv("GEMINI_API_KEY")
+api_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + api
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def get_response():
+    try:
+        course = request.get_json().get('course')
+        submodule=request.get_json().get('submodule')
+        target = request.get_json().get('target')
+        # module = request.get_json().get('module')
+        # submodules = request.get_json().get('submodules')
+        
+        # submodules = " , ".join(submodules)
+            # Define the request payload
+        payload = {
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": 
+                            f'''
+        This is the course {course} and the  submodule {submodule}
+            Can you create 3 examples for the submodule{submodule} to understand it better
+            Give the output  exapmpes in json format as code block:
+            {{
+                "examples":["strings"]
+            }}
+        '''
+                        }
+                    ]
+                }
+            ]
+        }
+
+        # Set the headers
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        # Make the API call
+        response = requests.post(api_url, json=payload, headers=headers)
+
+        if response.status_code == 200:
+            # Parse and print the response JSON
+            response_json = response.json()
+            #print(response_json["candidates"][0]["content"]["parts"][0]["text"][7:-3])
+            return (response_json["candidates"][0]["content"]["parts"][0]["text"][7:-3])
+            # print(json.dumps(response_json, indent=2))
+            # print(response_json)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return json.dump(
+            {
+                "msg":"Error Assessing AI"
+            }
+            ) 
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+# ... (your existing code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
